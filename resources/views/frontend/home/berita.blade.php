f1dc74c8de0163fec2b5e47f6fcb1f9a522163c5
andika1304 <dikxly13@gmail.com>
2023-02-20 08:04:22 +0700

    memasukan database

--- resources/views/frontend/home/berita.blade.php
+++ resources/views/frontend/home/berita.blade.php
@@ -2,10 +2,10 @@
      <div class="container">
          <h3 class="heading mb-5">Berita</h3>
          <div class="row ml-sm-5">
+            @forelse ($data['berita'] as $row)
              <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
                  <div class="our-services-wrapper mb-60">
                      <div class="services-inner">
-                         @forelse ($data['berita'] as $row)
                              <div class="our-services-img">
                                  <img src="{{ 'http://127.0.0.1:8000' . Storage::url('berita/') . $row->foto_b }}">
                              </div>
